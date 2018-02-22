a76ed4b4fba597be8bd4b90cbed931d84f39f14c
ureyni <hasan.ucak@gmail.com>
2018-02-22 11:13:27 +0300

    added explain line

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GoHazelcast
 hazelcast cache testing with golang.
+<b>hazelcast load test for cache.</b>
--- main.go
+++ main.go
@@ -1,3 +1,14 @@
+/*
+ * Created by goland
+ * User: Hasan UCAK <hasan.ucak@gmail.com>
+ * Date: 2/21/2018
+ * Time: 3:00 PM
+
+load test hazelcast for cache mechanism.
+
+*/
+
+
 package main
 
 import (
@@ -6,63 +17,75 @@ import (
 	"time"
 )
 
-func getKey(client hazelcast.IHazelcastInstance, i int,pi int) {
-	start := time.Now()
-	mp, _ := client.GetMap("simpleExample")
-	key := fmt.Sprintf("%s%d-%d", "key_", i,pi)
+
+//set hazelcast username
+const username string = "pttkep"
+//set hazelcast password of username
+const password string = "pttkep"
+//set hazelcast host ip and port number
+const hazelcast_url string = "10.145.172.13:5701"
+//thread number meanly Goroutines
+const totalRoutines int = 1000
+//number of insert key per thread
+const maxKeyNumber int = 10000
+//ttl value for key .key/value life time in second
+const keyLifeTime int = 10
+//map name for test
+const mapName string = "testMap"
+
+
+
+/*
+ * get value of key from hazelcast with Goroutines
+ */
+func GetKey(client hazelcast.IHazelcastInstance, index int,routineIndex int) {
+	startTime := time.Now()
+	mp, _ := client.GetMap(mapName)
+	key := fmt.Sprintf("%s%d-%d", "key_", routineIndex,index)
 	result1, _ := mp.Get(key)
 	fmt.Printf("\n %s has the value of %s", key,result1)
-	fmt.Printf("%s%d-%d", "get Key End:", i,pi)
-
-	//result1, _ := mp.Get("key1")
-	//fmt.Println("key1 has the value of ", result1)
-
-	//result2, _ := mp.Get("key2")
-	//fmt.Println("key2 has the value of ", result2)
-
-	//mp.Clear()
-	t := time.Now()
-	elapsed := t.Sub(start)
+	fmt.Printf("--%s%d-%d", "get Key End:", routineIndex,index)
+	//calculate duraction time
+	endTime := time.Now()
+	elapsed := endTime.Sub(startTime)
 	fmt.Printf("%d%s%d\n", i, " Duraction : ", elapsed.Seconds())
 }
 
-func setKey(client hazelcast.IHazelcastInstance, pi int) {
-	start := time.Now()
-	mp, _ := client.GetMap("simpleExample")
-	for i := 0; i < 10000; i++ {
-		mp.SetWithTtl(fmt.Sprintf("%s%d-%d", "key_", i,pi), fmt.Sprintf("%s%d-%d", "value_", i,pi), 10, time.Second)
-		go getKey(client,i,pi)
+/*
+ * set value of key from hazelcast with Goroutines
+ */
+func SetKey(client hazelcast.IHazelcastInstance, routineIndex int) {
+	startTime := time.Now()
+	mp, _ := client.GetMap(mapName)
+	for index := 0; index < maxKeyNumber; index++ {
+		mp.SetWithTtl(fmt.Sprintf("%s%d-%d", "key_", routineIndex,index), fmt.Sprintf("%s%d-%d", "value_", routineIndex,index), keyLifeTime, time.Second)
+		go GetKey(client,index,routineIndex)
 	}
-	fmt.Printf("%s%d", "Set Key End:", pi)
-
-	//result1, _ := mp.Get("key1")
-	//fmt.Println("key1 has the value of ", result1)
-
-	//result2, _ := mp.Get("key2")
-	//fmt.Println("key2 has the value of ", result2)
-
-	//mp.Clear()
-	t := time.Now()
-	elapsed := t.Sub(start)
-	fmt.Printf("%d%s%d\n", pi, " Duraction : ", elapsed.Seconds())
+	fmt.Printf("%s%d", "Set Key End:", routineIndex)
+	//calculate duraction time
+	endTime := time.Now()
+	elapsed := endTime.Sub(startTime)
+	fmt.Printf("%d%s%d\n", routineIndex, " Duraction : ", elapsed.Seconds())
 }
 
 func main() {
 	config := hazelcast.NewHazelcastConfig()
-	config.GroupConfig().SetName("pttkep")
-	config.GroupConfig().SetPassword("pttkep")
-	config.ClientNetworkConfig().AddAddress("10.145.172.13:5701")
-
+	//set hazelcast config
+	config.GroupConfig().SetName(username)
+	config.GroupConfig().SetPassword(password)
+	config.ClientNetworkConfig().AddAddress(hazelcast_url)
+	//starting test
 	client, err := hazelcast.NewHazelcastClientWithConfig(config)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	for i := 0; i < 100; i++ {
-		go setKey(client, i)
+	//tread number
+	for routineIndex := 0; routineIndex < totalRoutines; routineIndex++ {
+		go SetKey(client, routineIndex)
 	}
 	fmt.Print(" End Main ")
+	//we have to wait for the process to end
 	fmt.Scanln()
-	//client.Shutdown()
+	client.Shutdown()
 }
